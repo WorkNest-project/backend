b624d429ee784846861974c8a4eb6cb10153ad55
Barmatov Alexey <alexrui_hpow@mail.ru>
2025-04-24 18:53:18 +0300

    feat: init project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { WebsocketGateway } from './websocket/websocket.gateway';
+import { NotificationsModule } from './notifications/notifications.module';
+import { BillingModule } from './billing/billing.module';
+import { CommentsModule } from './comments/comments.module';
+import { ColumnsModule } from './columns/columns.module';
+import { TasksModule } from './tasks/tasks.module';
+import { BoardsModule } from './boards/boards.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    BoardsModule,
+    TasksModule,
+    ColumnsModule,
+    CommentsModule,
+    BillingModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WebsocketGateway],
 })
 export class AppModule {}
